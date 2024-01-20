72681f2ad21e84663c83d36eda2dd29b2934b56d
Labeyrie Louis <louis.labeyrie@fluxodigital.co>
2024-01-20 16:47:47 +0100

    Add authentication routes

---
+++ backend/api/controllers/authControler.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express';
+import { PrismaClient, type User } from '@prisma/client';
+import { expressjwt, Request as JWTRequest } from 'express-jwt';
+import { sign } from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
+const SECRET = 'secret';
+
+const jwt = expressjwt({
+	secret: SECRET,
+	algorithms: ['HS256'],
+}).unless({ path: ['/login', '/register', '/user'] });
+
+export const login = async (req: JWTRequest, res: Response) => {
+	const { email, password } = req.body;
+
+	const user = await prisma.user.findUnique({
+		where: {
+			email,
+            password,
+		},
+	});
+
+    console.log(user);
+
+	if (user) {
+		const token = sign({ email: user.email, id: user.id }, SECRET, {
+			expiresIn: '1h',
+		});
+		return res.status(200).json({ token });
+	} else {
+		return res.status(401).json({ message: 'Invalid credentials' });
+	}
+};
+
+export const register = async (req: Request, res: Response) => {
+	const { email, password, name } = req.body;
+
+	// check if all parameters are sent
+    if (email === undefined || password === undefined || name === undefined) {
+        return res.status(400).json({ message: 'Missing parameters' });
+    }
+
+    // check if user already exists
+    const userFound = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (userFound) {
+        return res.status(409).json({ message: 'User already exists' });
+    } else {
+        const newUser = {
+            name,
+            email,
+            password,
+        };
+        await prisma.user.create({
+            data: newUser,
+        });
+
+        return res.status(201).json({ message: 'User created' });
+    }
+};
--- backend/api/src/app.ts
+++ backend/api/src/app.ts
@@ -5,15 +5,27 @@ import userRoutes from '../routes/userRoutes';
 import splitRoutes from '../routes/splitRoutes';
 import expenseRoutes from '../routes/expenseRoutes';
 import shareRoutes from '../routes/shareRoutes';
+import authRoutes from '../routes/authRoutes';
+import { expressjwt } from 'express-jwt';
+
+const SECRET = 'secret';
+
+const jwt = expressjwt({
+	secret: SECRET,
+	algorithms: ['HS256'],
+}).unless({ path: ['/login', '/register'] });
 
 const app: Express = express();
 
+app.use(jwt);
 app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use('/users', userRoutes);
 app.use('/splits', splitRoutes);
 app.use('/expenses', expenseRoutes);
 app.use('/shares', shareRoutes);
+app.use(authRoutes);
 
 export default app;
